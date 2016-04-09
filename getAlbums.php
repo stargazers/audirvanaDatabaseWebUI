c75b3a69339eae1ce4c4fa93367af3c1fd2ee57f
Aleksi Räsänen <aleksi.rasanen@runosydan.net>
2016-04-10 00:44:17 +0300

    Track titles to URL.

--- getAlbums.php
+++ getAlbums.php
@@ -1,6 +1,8 @@
 <?php
 
 	$cover_images_path = 'covers/';
+	$original_path = 'Users/stargazers/Music/';
+	$new_path = 'http://media.korpilaakso.net/';
 
 	$db = new SQLite3( 'AudirvanaPlusLibrary.sqlite', true )
 		or die( "Fuck this shit" );
@@ -10,15 +12,23 @@
 
 	while( $row = $ret->fetchArray())
 	{
-		$q = 'SELECT ZTRACKNUMBER, ZPLAYCOUNT, ZTITLE FROM ZTRACK WHERE ZALBUM="' . $row['Z_PK'] . '" ORDER BY ZTRACKNUMBER';
+		// Get tracks
+		$q = 'SELECT ZTRACKNUMBER, ZPLAYCOUNT, ZTITLE, ZLOCATIONRELPATH '
+			. 'FROM ZTRACK WHERE ZALBUM="' . $row['Z_PK'] . '"'
+			. 'ORDER BY ZTRACKNUMBER';
+
 		$track_ret = $db->query( $q );
 		$tracks = '';
 
+		// Add tracks array to this row
 		while( $track_row = $track_ret->fetchArray())
 		{
+			$file_url = str_replace( $original_path, $new_path, $track_row['ZLOCATIONRELPATH'] );
+
 			$track['number'] = $track_row['ZTRACKNUMBER'];
 			$track['title'] = $track_row['ZTITLE'];
 			$track['playcount'] = $track_row['ZPLAYCOUNT'];
+			$track['url'] = $file_url;
 
 			$tracks[] = $track;
 		}
